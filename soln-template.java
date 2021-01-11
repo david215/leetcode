d6e63adb7f249af8e4be63bf073e8c9c8bd809e9
David Chung <david215@berkeley.edu>
2021-01-12 00:06:53 +0900

    [LC] Add templates

---
+++ soln-template.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    
+    // method signature
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
---
+++ write-up-template.md
@@ -0,0 +1,22 @@
+[Problem]()
+
+## constraints
+-
+
+## insight
+-
+
+## edge cases
+-
+
+## submission 1
+```java
+
+```
+- Time
+    - O(?)
+- Space
+    - O(?)
+- Result
+    - ?
+
